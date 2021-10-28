02329934b2291ea48e4d7870ffececa55aafd970
JohnnySD <ba004629@bac.qld.edu.au>
2021-10-28 14:00:00 +1000

    Update

---
+++ docs/scripts/index.js
@@ -0,0 +1,8 @@
+var location = window.location.href;
+if (a === undefined) {
+	window.location.href = location + "?a";
+} else if (a === null) {
+	
+} else {
+	document.getElementById("page") = '<html lang="en" dir="ltr"><head><meta charset="utf-8" http-equiv="refresh" content="0; url=turnipguy30.html"><link rel="icon" href="images/celebi.png"><title>'+a+' | Celebi</title><style>@import url("/styles/index.css");</style></head><body><h1>'+a+'</h1><img src="https://gpvc.arturio.dev/'+a+'"></body></html>';
+};
