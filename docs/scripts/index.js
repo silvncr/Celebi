c452c3dca6a4b511c1a0b5e076e62236ebebbb85
JohnnySD <ba004629@bac.qld.edu.au>
2021-10-28 14:11:35 +1000

    Update

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -4,7 +4,7 @@ if (a === undefined) {
 } else if (a === null) {
 
 } else {
-	document.getElementById("page") = '<html lang="en" dir="ltr"><head><meta charset="utf-8" http-equiv="refresh" content="0; url=turnipguy30.html"><link rel="icon" href="images/celebi.png"><title>'+a+' | Celebi</title><style>@import url("/styles/index.css");</style></head><body><h1>'+a+'</h1><img src="https://gpvc.arturio.dev/'+a+'"></body></html>';
+	document.getElementById("page").innerHTML = '<html lang="en" dir="ltr"><head><meta charset="utf-8" http-equiv="refresh" content="0; url=turnipguy30.html"><link rel="icon" href="images/celebi.png"><title>'+a+' | Celebi</title><style>@import url("/styles/index.css");</style></head><body><h1>'+a+'</h1><img src="https://gpvc.arturio.dev/'+a+'"></body></html>';
 };
 submit.onClick = function() {
 	window.location.href = location + "=" + document.getElementById("input").content;
